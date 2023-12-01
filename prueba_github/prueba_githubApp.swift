249210d1a73ae54f65cd9518d624dee7da4a9cd0
cmulero83 <cmulero83@icloud.com>
2023-12-01 13:46:27 -0600

    Initial Commit

---
+++ prueba_github/prueba_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  prueba_githubApp.swift
+//  prueba_github
+//
+//  Created by Carlos Mulero on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct prueba_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
